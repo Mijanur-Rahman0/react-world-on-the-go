b364e0313fb1ea46ae4bf004a5f895241d9faf0c
Mijanur Rahman <mijanurrahman0645@gmail.com>
2026-03-29 01:37:08 +0600

    Add area and add a button that show do i visit any country or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./components/Countries/Countries";
 
-import './App.css'
-import Countries from './components/Countries/Countries'
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all",
+).then((res) => res.json());
 
 function App() {
-
-
   return (
     <>
-      
-      <Countries></Countries>
-
+      <Suspense fallback={<p>Countries are loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './Country.css';
+
+const Country = ({country}) => {
+    const [visited, setVisited] = useState(false);
+    // console.log(country.name.common);
+
+    const handleVisited = () => {
+        // setVisited(true);
+
+        // basic system
+
+        // if(visited){
+        //     setVisited(false);
+        // }
+        // else{
+        //     setVisited(true);
+        // }
+
+        // second system
+        // setVisited(visited ? false : true);
+
+        // third system
+        setVisited(!visited);
+    }
+    return (
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Region: {country.region.region}</p>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
+        </div>
+    );
+};
+
+export default Country;
