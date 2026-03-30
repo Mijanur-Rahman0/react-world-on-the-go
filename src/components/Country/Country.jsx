4652ae9955eae109b68f708461b39e8e90e4e07d
Mijanur Rahman <mijanurrahman0645@gmail.com>
2026-03-30 10:48:30 +0600

    Lift up state and pass handle as props

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries, handleVisitedFlag}) => {
     const [visited, setVisited] = useState(false);
     // console.log(country.name.common);
 
@@ -22,15 +22,23 @@ const Country = ({country}) => {
 
         // third system
         setVisited(!visited);
+        handleVisitedCountries(country);
     }
+
+    // const handleVisitedFlag = () => {
+    //     console.log('flag need to be added')
+    //     handleVisitedFlag(country.flags.flags.png);
+    // }
+    
     return (
         <div className={`country ${visited && 'country-visited'}`}>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Region: {country.region.region}</p>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
+            <button onClick={() => handleVisitedFlag(country?.flags?.flags?.png)}>Add Visited Flag</button>
         </div>
     );
 };
